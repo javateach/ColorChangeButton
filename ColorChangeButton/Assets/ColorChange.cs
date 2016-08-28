89ed2f27c9609a81abdbf2f05b56a80bdc57addd
Diane Christie <christied@uwstout.edu>
2016-08-28 11:10:09 -0500

    initial upload

---
+++ ColorChangeButton/Assets/ColorChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorChange : MonoBehaviour {
+
+    public Image background;
+
+	public void ChangeBackgroundColor()
+    {
+        background.color = Color.blue;
+    }
+}
